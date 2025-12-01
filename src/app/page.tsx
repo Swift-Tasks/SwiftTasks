fb10db1307aee6eae7d3a897169f5a177790d4b3
bsdc-george <30258127@bsdc.ac.uk>
2025-12-01 12:01:48 +0000

    Logo in coror homepage properly aligned - George C 12:01 01/12/25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ export default function HomePage() {
 
   return (
     // Insert within the brackets.
-    <div className="text-red-500 bg-cyan-800">
-    <img className="w-[300px]" src="/SwifTaskLogo.png" alt="Logo" />
+    <div>
+    <img className="w-[150px]" src="/SwifTaskLogo.png" alt="Logo" />
 
       <h1>Hello Gregory</h1>
 
