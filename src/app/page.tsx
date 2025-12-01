8df4dd8215eb698ace8b1cfebb4b3372a6f263eb
bsdc-george <30258127@bsdc.ac.uk>
2025-12-01 11:23:33 +0000

    Added Logo to public - George C 01/12/25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ export default function HomePage() {
   return (
     // Insert within the brackets.
     <div className="text-red-500 bg-cyan-800">
-      <h1>Hello Alanay</h1>
+      <h1>Hello Gregory</h1>
+      
     </div>
   )
 }
