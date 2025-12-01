91a12aea8336e29c2a3e7f6b1286ed32b541efc0
bsdc-george <30258127@bsdc.ac.uk>
2025-12-01 14:37:01 +0000

    centered Hello User to the page and the logo - George C 14:36 01/12/25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ export default function HomePage() {
 
   return (
     // Insert within the brackets.
-    <div>
+    <div className="w-screen flex items-center gap-150">
     <img className="w-[150px]" src="/SwifTaskLogo.png" alt="Logo" />
 
-      <h1>Hello Gregory</h1>
+      <h1>Hello, User!</h1>
 
     </div>
   )
