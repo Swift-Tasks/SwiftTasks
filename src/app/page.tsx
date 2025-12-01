c7d0339ac06b28bc6d10ec763e5abf3feb19df75
bsdc-george <30258127@bsdc.ac.uk>
2025-12-01 11:39:43 +0000

    Example learning - George C 01/12/25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ export default function HomePage() {
   return (
     // Insert within the brackets.
     <div className="text-red-500 bg-cyan-800">
+    <img className="w-[300px]" src="/SwifTaskLogo.png" alt="Logo" />
+
       <h1>Hello Gregory</h1>
-      
+
     </div>
   )
 }
