7c6f8e9c653891c4753998dc2df8f30fb2bd230f
William Banham <will_banh@icloud.com>
2025-12-02 14:10:12 +0000

    Added database & ORM, along with typesafe environment variables - will - 14:10 2-12-25

---
+++ src/env.ts
@@ -0,0 +1,16 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    NODE_ENV: z.enum(["development", "test", "production"]),
+    DB_FILE: z.string(),
+  },
+  client: {
+    // NEXT_PUBLIC_CLIENTVAR: z.string(),
+  },
+  runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+    DB_FILE: process.env.DB_FILE,
+  },
+});
---
+++ src/server/db/index.ts
@@ -0,0 +1,12 @@
+import { env } from "@/env";
+import { drizzle } from "drizzle-orm/libsql";
+
+declare global {
+  var db: ReturnType<typeof drizzle> | undefined;
+}
+
+export const dbInstance = global.db ?? drizzle(env.DB_FILE!);
+
+if (!global.db) {
+  global.db = dbInstance;
+}
