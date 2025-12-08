19a401a95246a7859ba3d2dc9a982450c902999f
bsdc-alanya <30271090@bsdc.ac.uk>
2025-12-08 09:37:37 +0000

    Fixed changes to Title on Lits Page - Alanya - 08/12

--- src/app/dashboard/tasks/[id]/page.tsx
+++ src/app/dashboard/tasks/[id]/page.tsx
@@ -1,6 +1,15 @@
-export default function TaskPage() {
+export default function DashboardPage() {
   return (
     // Insert within the brackets.
-    <div></div>
+    <div className="overflow-hidden">
+      <div className="flex w-screen flex-col  h-screen overflow-hidden items-center justify-center">
+
+        <div className="w-[75%] p-3 h-[75%] bg-white">
+                  <h1 contentEditable className="w-full">
+          Cyber Security
+        </h1>
+        </div>
+      </div>
+    </div>
   );
 }
