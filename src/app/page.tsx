a71f031973ad362f4932fe6c395e4b807d041136
bsdc-george <30258127@bsdc.ac.uk>
2025-12-08 09:49:14 +0000

    Changed the size of the 'Hello User' Text and moved it outside its Div - George C - 09:49 08/12/25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export default function HomePage() {
       <div className="w-screen flex items-center justify-between gap-4 pt-4 px-4">
         <img className="w-[150px]" src="/images/SwifTaskLogo.png" alt="Logo" />
 
-        <h1>Hello, User!</h1>
       <a href="/dashboard" className="block">
   <Image 
     className="w-12 h-12 hover:scale-120 hover:brightness-120 transition-all duration-200 cursor-pointer"
@@ -18,12 +17,13 @@ export default function HomePage() {
   />
 </a>
       </div>
+        <h1 className="text-6xl">Hello, User!</h1>
 
       <div className="flex justify-center items-center h-[calc(100vh-250px)] gap-8 px-12">
         {/* <link href="/dashboard"> */}
         <div className="max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow min-h-[500px]">
           <h2 className="text-xl font-semibold mb-4 flex  items-center gap-2">
-            <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"  className="text-[#FFB900] lucide lucide-clipboard-clock-icon lucide-clipboard-clock"><path d="M16 14v2.2l1.6 1"/><path d="M16 4h2a2 2 0 0 1 2 2v.832"/><path d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h2"/><circle cx="16" cy="16" r="6"/><rect x="8" y="2" width="8" height="4" rx="1"/></svg>
+            <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" stroke-linecap="round" stroke-linejoin="round"  className="text-[#FFB900] lucide lucide-clipboard-clock-icon lucide-clipboard-clock"><path d="M16 14v2.2l1.6 1"/><path d="M16 4h2a2 2 0 0 1 2 2v.832"/><path d="M8 4H6a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h2"/><circle cx="16" cy="16" r="6"/><rect x="8" y="2" width="8" height="4" rx="1"/></svg>
             Assignments Due:
           </h2>
           <div className="space-y-3">
